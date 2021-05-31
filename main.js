ca1150f35900e6abd872c4652b3dfdc8b0b80b85
atcoolart <atcooltruewook@gmail.com>
2021-05-31 13:13:27 +0700

    Test blockchain
    Testing Blockchain previousHash.

--- main.js
+++ main.js
@@ -7,10 +7,40 @@ class Block {
 		this.timestamp = timestamp;
 		this.data = data;
 		this.previousHash = previousHash;
-		this.hash = '';
+		this.hash = this.calculateHash();
 	}
 
 	calculateHash() {
 		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 	}
 }
+
+
+class Blockchain {
+	constructor (){
+		this.chain = [this.createGenesisBlock()];
+	}
+
+	createGenesisBlock()
+	{
+		return new Block(0, "01/06/2021", "Genesis Block","0");
+	}
+
+	getLastestBlock() {
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(newBlock){
+		newBlock.previousHash = this.getLastestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	}
+}
+
+
+let FUSD = new Blockchain();
+FUSD.addBlock(new Block(1, "01/06/2021", { amount : 1000000}));
+FUSD.addBlock(new Block(2, "02/06/2021", { amount : 1000000}));
+
+
+console.log(JSON.stringify(FUSD, null, 4));
